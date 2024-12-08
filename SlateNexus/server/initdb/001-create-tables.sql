79396fb9bade98dccd75dd356deaedcbde4017fe
slate20 <155096805+slate20@users.noreply.github.com>
2024-12-07 19:11:47 -0600

    Cleaned up repo

---
+++ SlateNexus/server/initdb/001-create-tables.sql
@@ -0,0 +1,36 @@
+-- Creat the Host Information table
+CREATE TABLE IF NOT EXISTS agents (
+    host_id SERIAL PRIMARY KEY,
+    hostname VARCHAR(255) NOT NULL,
+    ip_address VARCHAR(15),
+    os VARCHAR(50),
+    os_version VARCHAR(50),
+    hardware_specs JSONB,
+    agent_version VARCHAR(50),
+    last_seen TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    last_user VARCHAR(255)
+);
+
+-- Create the device_groups Table
+CREATE TABLE IF NOT EXISTS device_groups (
+    group_id SERIAL PRIMARY KEY,
+    group_name VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Create the device_group_members Table
+CREATE TABLE IF NOT EXISTS device_group_members (
+    host_id INT NOT NULL,
+    group_id INT NOT NULL,
+    PRIMARY KEY (host_id, group_id),
+    FOREIGN KEY (host_id) REFERENCES agents(host_id),
+    FOREIGN KEY (group_id) REFERENCES device_groups(group_id)
+);
+
+-- Create the User Table
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
--- agent/go.mod
+++ agent/go.mod
@@ -10,5 +10,5 @@ require (
 	github.com/tklauser/go-sysconf v0.3.13 // indirect
 	github.com/tklauser/numcpus v0.7.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/sys v0.17.0 // direct
 )
